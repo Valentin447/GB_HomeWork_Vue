3465e337bdd8485780a5f6ac20d4d0d02d452aea
Valentin447 <valentin@myttk.ru>
2023-10-20 15:00:52 +0700

    Home work 10

---
+++ Lesson3/app/src/components/ProductDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product">
+    <h4 class="name">
+      {{ product.name }}
+    </h4>
+    <p>Price: {{ formattedPrice }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        product:{
+          name: "pen",
+          price: 99.99,
+          available: true,
+        },
+      }
+    },
+    computed:{
+      formattedPrice(){
+        return `$${this.product.price}`;
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
